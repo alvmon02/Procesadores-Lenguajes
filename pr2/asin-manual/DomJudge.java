bf98da07a322987696f333a178d85c1dfc068e53
SuperDanUCM <117196575+DanielLopezUCM@users.noreply.github.com>
2025-02-27 22:38:45 +0100

    renombre del analizadorSintaticoDJ

--- pr2/asin-manual/DomJudge.java
+++ pr2/asin-manual/DomJudge.java
@@ -1,12 +1,12 @@
 import asint.AnalizadorSintacticoTiny;
-import asint.AnalizadorSintacticoEvalDJ;
+import asint.AnalizadorSintacticoTinyDJ;
 import errors.GestionErroresTiny.ErrorLexico;
 import errors.GestionErroresTiny.ErrorSintactico;
 import java.io.InputStreamReader;
 public class DomJudge{
    public static void main(String[] args) throws Exception {
      try{  
-      AnalizadorSintacticoTiny asint = new AnalizadorSintacticoEvalDJ(new InputStreamReader(System.in));
+      AnalizadorSintacticoTiny asint = new AnalizadorSintacticoTinyDJ(new InputStreamReader(System.in));
       asint.analiza();
      }
      catch(ErrorSintactico e) {
--- pr2/asin-manual/asint/AnalizadorSintacticoTiny.java
+++ pr2/asin-manual/asint/AnalizadorSintacticoTiny.java
@@ -12,173 +12,226 @@ import java.util.logging.Level;
 import java.util.logging.Logger;
 
 public class AnalizadorSintacticoTiny {
-   private UnidadLexica anticipo;       // token adelantado
-   private AnalizadorLexicoTiny alex;   // analizador léxico 
-   private GestionErroresTiny errores;  // gestor de errores
-   private Set<ClaseLexica> esperados;  // clases léxicas esperadas
-   
-   public AnalizadorSintacticoTiny(Reader input) throws IOException {
+    private UnidadLexica anticipo; // token adelantado
+    private AnalizadorLexicoTiny alex; // analizador léxico
+    private GestionErroresTiny errores; // gestor de errores
+    private Set<ClaseLexica> esperados; // clases léxicas esperadas
+
+    public AnalizadorSintacticoTiny(Reader input) throws IOException {
         // se crea el gestor de errores
-      errores = new GestionErroresTiny();
+        errores = new GestionErroresTiny();
         // se crea el analizador léxico
-      alex = new AnalizadorLexicoTiny(input,errores);
-      esperados = EnumSet.noneOf(ClaseLexica.class);
+        alex = new AnalizadorLexicoTiny(input, errores);
+        esperados = EnumSet.noneOf(ClaseLexica.class);
         // Se lee el primer token adelantado
-      sigToken();                      
-   }
-   public void analiza() {
-      programa();
-      empareja(ClaseLexica.EOF);
-   }
-   private void programa() {
-       expresion();
-       declaraciones();
-   }
-   
-   private void expresion() {
-       empareja(ClaseLexica.EVALUA);
-       E0();
-   }
-   
-   private void declaraciones() {
-       switch(anticipo.clase()) {
-           case DONDE: 
-                 empareja(ClaseLexica.DONDE);
-                 lista_declaraciones();
-                 break;
-           default:        
-                esperados(ClaseLexica.DONDE);
+        sigToken();
+    }
+
+    public void analiza() {
+        programa();
+        empareja(ClaseLexica.EOF);
+    }
+
+    private void programa() {
+        empareja(ClaseLexica.LLAVEAP);
+        declaraciones();
+        instrucciones();
+        empareja(ClaseLexica.LLAVECIERRE);
+    }
+
+    private void declaraciones() {
+        switch (anticipo.clase()) {
+            case P_INT:
+            case P_BOOL:
+            case P_REAL:
+                lista_declaraciones();
+                empareja(ClaseLexica.FINDECLAR);
                 break;
-       } 
-   }
-   
-   private void lista_declaraciones() {
-       declaracion();
-       rlista_decs();
-   }
-   
-   private void declaracion() {
-       empareja(ClaseLexica.IDEN);
-       empareja(ClaseLexica.IGUAL);
-       E0();
-   }    
-   
-   private void rlista_decs() {
-       switch(anticipo.clase()) {
-           case COMA: 
-               empareja(ClaseLexica.COMA);
-               declaracion();
-               rlista_decs();
-               break;
-           default:
-              esperados(ClaseLexica.COMA);
-              break;
-       }
-   }
-   
-   private void E0() {
-       E1();
-       RE0();
-   }
-   
-   private void RE0() {
-       switch(anticipo.clase()) {
-           case MAS: case MENOS: 
-               OP0();
-               E1();
-               RE0();
-               break;
-           default: 
-              esperados(ClaseLexica.MAS,ClaseLexica.MENOS);
-              break;
-       }    
+            default:
+                esperados(ClaseLexica.P_INT, ClaseLexica.P_BOOL, ClaseLexica.P_REAL);
+                break;
+        }
     }
-   
 
-   private void E1() {
-       E2();
-       RE1();
-   }
-   
-   private void RE1() {
-       switch(anticipo.clase()) {
-           case POR: case DIV: 
-               OP1();
-               E2();
-               RE1();
-               break;
-           default: 
-              esperados(ClaseLexica.POR,ClaseLexica.DIV);
-              break;
-       }    
+    private void lista_declaraciones() {
+        declaracion();
+        rlista_decs();
     }
-   
-   private void E2() {
-      switch(anticipo.clase()) {
-          case ENT: empareja(ClaseLexica.ENT); break;
-          case REAL: empareja(ClaseLexica.REAL); break; 
-          case IDEN: empareja(ClaseLexica.IDEN); break;
-          case PAP: 
-               empareja(ClaseLexica.PAP); 
-               E0(); 
-               empareja(ClaseLexica.PCIERRE); 
-               break;
-          default:
-              esperados(ClaseLexica.ENT,ClaseLexica.REAL,ClaseLexica.IDEN,ClaseLexica.PAP);
-              error();
-   }   
-   }
-   
-   private void OP0() {
-     switch(anticipo.clase()) {
-         case MAS: empareja(ClaseLexica.MAS); break;  
-         case MENOS: empareja(ClaseLexica.MENOS); break;  
-         default:    
-              esperados(ClaseLexica.MAS,ClaseLexica.MENOS);             
-              error();
-     }  
-   }
-   private void OP1() {
-     switch(anticipo.clase()) {
-         case POR: empareja(ClaseLexica.POR); break;  
-         case DIV: empareja(ClaseLexica.DIV); break;  
-         default:    
-             esperados(ClaseLexica.POR,ClaseLexica.DIV);             
-             error();
-     }  
-   }
-       
 
-   private void esperados(ClaseLexica ... esperadas) {
-       for(ClaseLexica c: esperadas) {
-           esperados.add(c);
-       }
-   }
-   
-   
-   private void empareja(ClaseLexica claseEsperada) {
-      if (anticipo.clase() == claseEsperada) {
-          traza_emparejamiento(anticipo);
-          sigToken();
-      }    
-      else {
-          esperados(claseEsperada);
-          error();
-      }
-   }
-   private void sigToken() {
-      try {
-        anticipo = alex.sigToken();
-        esperados.clear();
-      }
-      catch(IOException e) {
-        errores.errorFatal(e);
-      }
-   }
-   
+    private void tipo() {
+        switch (anticipo.clase()) {
+            case P_INT:
+                empareja(ClaseLexica.P_INT);
+                break;
+            case P_BOOL:
+                empareja(ClaseLexica.P_BOOL);
+                break;
+            case P_REAL:
+                empareja(ClaseLexica.P_REAL);
+                break;
+            default:
+                esperados(ClaseLexica.P_INT, ClaseLexica.P_BOOL, ClaseLexica.P_REAL);
+                error();
+        }
+    }
+
+    private void declaracion() {
+        switch (anticipo.clase()) {
+            case P_INT:
+            case P_BOOL:
+            case P_REAL:
+                tipo();
+                empareja(ClaseLexica.IDEN);
+            default:
+                esperados(ClaseLexica.P_INT, ClaseLexica.P_BOOL, ClaseLexica.P_REAL);
+                error();
+        }
+    }
+
+    private void rlista_decs() {
+        switch (anticipo.clase()) {
+            case PCOMA:
+                empareja(ClaseLexica.PCOMA);
+                declaracion();
+                rlista_decs();
+                break;
+            default:
+                esperados(ClaseLexica.PCOMA);
+                break;
+        }
+    }
+
+    private void instrucciones() {
+        switch (anticipo.clase()) {
+            case INIEVAL:
+                lista_instrucciones();
+                break;
+            default:
+                esperados(ClaseLexica.INIEVAL);
+                break;
+        }
+    }
+
+    private void lista_instrucciones() {
+    }
+
+    private void E0() {
+        E1();
+        RE0();
+    }
+
+    private void RE0() {
+        switch (anticipo.clase()) {
+            case MAS:
+            case MENOS:
+                OP0();
+                E1();
+                RE0();
+                break;
+            default:
+                esperados(ClaseLexica.MAS, ClaseLexica.MENOS);
+                break;
+        }
+    }
+
+    private void E1() {
+        E2();
+        RE1();
+    }
+
+    private void RE1() {
+        switch (anticipo.clase()) {
+            case POR:
+            case DIV:
+                OP1();
+                E2();
+                RE1();
+                break;
+            default:
+                esperados(ClaseLexica.POR, ClaseLexica.DIV);
+                break;
+        }
+    }
+
+    private void E2() {
+        switch (anticipo.clase()) {
+            case ENT:
+                empareja(ClaseLexica.ENT);
+                break;
+            case REAL:
+                empareja(ClaseLexica.REAL);
+                break;
+            case IDEN:
+                empareja(ClaseLexica.IDEN);
+                break;
+            case PAP:
+                empareja(ClaseLexica.PAP);
+                E0();
+                empareja(ClaseLexica.PCIERRE);
+                break;
+            default:
+                esperados(ClaseLexica.ENT, ClaseLexica.REAL, ClaseLexica.IDEN, ClaseLexica.PAP);
+                error();
+        }
+    }
+
+    private void OP0() {
+        switch (anticipo.clase()) {
+            case MAS:
+                empareja(ClaseLexica.MAS);
+                break;
+            case MENOS:
+                empareja(ClaseLexica.MENOS);
+                break;
+            default:
+                esperados(ClaseLexica.MAS, ClaseLexica.MENOS);
+                error();
+        }
+    }
+
+    private void OP1() {
+        switch (anticipo.clase()) {
+            case POR:
+                empareja(ClaseLexica.POR);
+                break;
+            case DIV:
+                empareja(ClaseLexica.DIV);
+                break;
+            default:
+                esperados(ClaseLexica.POR, ClaseLexica.DIV);
+                error();
+        }
+    }
+
+    private void esperados(ClaseLexica... esperadas) {
+        for (ClaseLexica c : esperadas) {
+            esperados.add(c);
+        }
+    }
+
+    private void empareja(ClaseLexica claseEsperada) {
+        if (anticipo.clase() == claseEsperada) {
+            traza_emparejamiento(anticipo);
+            sigToken();
+        } else {
+            esperados(claseEsperada);
+            error();
+        }
+    }
+
+    private void sigToken() {
+        try {
+            anticipo = alex.sigToken();
+            esperados.clear();
+        } catch (IOException e) {
+            errores.errorFatal(e);
+        }
+    }
+
     private void error() {
         errores.errorSintactico(anticipo.fila(), anticipo.columna(), anticipo.clase(), esperados);
     }
-    
-    protected void traza_emparejamiento(UnidadLexica anticipo) {}  
+
+    protected void traza_emparejamiento(UnidadLexica anticipo) {
+    }
 }
