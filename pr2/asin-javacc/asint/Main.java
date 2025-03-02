6a79ad407a5b56a3d0fd6d45e39ae92959dd9a63
SuperDanUCM <117196575+DanielLopezUCM@users.noreply.github.com>
2025-03-02 14:51:24 +0100

    Actualizacion del las spec del javacc

--- pr2/asin-javacc/asint/Main.java
+++ pr2/asin-javacc/asint/Main.java
@@ -1,6 +1,8 @@
+package asint;
 
 import java.io.FileReader;
-public class Main{
+
+public class Main {
    public static void main(String[] args) throws Exception {
       AnalizadorSintacticoTiny asint = new AnalizadorSintacticoTiny(new FileReader(args[0]));
       // asint.disable_tracing();
