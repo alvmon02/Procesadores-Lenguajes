c9227f62af004b10a4592478cb19461459ec2ef1
DanielLopezUCM <117196575+DanielLopezUCM@users.noreply.github.com>
2025-03-03 12:00:39 +0100

    Added la suite de test de javacc

---
+++ pr2/asin-javacc/TestCasos.java
@@ -0,0 +1,42 @@
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.PrintStream;
+import java.io.Reader;
+
+import asint.*;
+
+public class TestCasos {
+    public static void main(String[] args) throws FileNotFoundException, IOException, Exception {
+        String directorioPath = "./casos"; // Puedes cambiarlo por el path deseado
+        File directorio = new File(directorioPath);
+
+        String[] archivos = directorio.list((dir, name) -> name.endsWith(".in"));
+
+        PrintStream salidaOriginal = System.out;
+
+        if (archivos != null) {
+            for (String archivo : archivos) {
+                String outputName = archivo.substring(0, archivo.lastIndexOf('.')) + ".txt";
+                Reader input = new InputStreamReader(new FileInputStream(directorioPath + "/" + archivo));
+                PrintStream output = new PrintStream(new FileOutputStream(directorioPath + "/" + outputName));
+                System.setOut(output);
+                try {
+                    AnalizadorSintacticoTiny asint = new AnalizadorSintacticoTinyDJ(input);
+                    asint.analiza();
+                } catch (ParseException e) {
+                    System.out.println("ERROR_SINTACTICO");
+                } catch (TokenMgrError e) {
+                    System.out.println("ERROR_LEXICO");
+                }
+                output.close();
+            }
+        } else {
+            System.err.println("El directorio no existe o no es accesible.");
+        }
+        System.setOut(salidaOriginal);
+    }
+}
