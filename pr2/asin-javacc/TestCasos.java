8d5a472cad3f486e2e8558cc534e8503d77d9e30
alvmon02 <alvmon02@ucm.es>
2025-03-06 12:00:26 +0100

    TestCasos.java de Javacc y CUP listos para entrega. eliminado archivo de ejemplo spec_evalua.jj

--- pr2/asin-javacc/TestCasos.java
+++ pr2/asin-javacc/TestCasos.java
@@ -2,15 +2,28 @@ import java.io.File;
 import java.io.FileInputStream;
 import java.io.FileNotFoundException;
 import java.io.FileOutputStream;
+import java.io.OutputStream;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.io.PrintStream;
 import java.io.Reader;
+import java.io.ByteArrayOutputStream;
 
 import asint.*;
 
 public class TestCasos {
+
     public static void main(String[] args) throws FileNotFoundException, IOException, Exception {
+
+        if (args.length == 0) // Pruebas con archivos de tipo '.in' en un directorio aparte
+            main_casos(args);
+        else if (args.length == 1) { // Acepta un único argumento, un archivo a provesar
+            main_archivo(args);
+        } else
+            System.err.println("ERROR: Númrero incorrecto de argumentos");
+    }
+
+    public static void main_casos(String[] args) throws FileNotFoundException, IOException, Exception {
         String directorioPath = "./casos"; // Puedes cambiarlo por el path deseado
         File directorio = new File(directorioPath);
 
@@ -39,4 +52,49 @@ public class TestCasos {
         }
         System.setOut(salidaOriginal);
     }
+
+    public static void main_archivo(String[] args) throws FileNotFoundException, IOException, Exception {
+        PrintStream salida = System.out;
+        Reader input = new InputStreamReader(new FileInputStream(args[0]));
+        try {
+            AnalizadorSintacticoTiny asint = new AnalizadorSintacticoTinyDJ(input);
+            System.setOut(new NullPrintStream());
+            asint.analiza();
+            System.setOut(salida);
+        } catch (ParseException e) {
+            System.setOut(salida);
+            System.out.println("ERROR_SINTACTICO");
+        } catch (TokenMgrError e) {
+            System.setOut(salida);
+            System.out.println("ERROR_LEXICO");
+        }
+        System.out.println("OK");
+    }
+
+    private static class NullPrintStream extends PrintStream {
+
+        public NullPrintStream() {
+            super(new NullByteArrayOutputStream());
+        }
+
+        private static class NullByteArrayOutputStream extends ByteArrayOutputStream {
+
+            @Override
+            public void write(int b) {
+                // do nothing
+            }
+
+            @Override
+            public void write(byte[] b, int off, int len) {
+                // do nothing
+            }
+
+            @Override
+            public void writeTo(OutputStream out) throws IOException {
+                // do nothing
+            }
+        }
+
+    }
+
 }
