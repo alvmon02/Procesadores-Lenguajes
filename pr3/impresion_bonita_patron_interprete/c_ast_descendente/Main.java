a2c363ba26a8bb2f02831ce02d61332845d2e308
Nacho <ignacg09@ucm.es>
2025-03-22 12:53:56 +0100

    algunos cambios intérprete

--- pr3/impresion_bonita_patron_interprete/c_ast_descendente/Main.java
+++ pr3/impresion_bonita_patron_interprete/c_ast_descendente/Main.java
@@ -2,8 +2,9 @@ package c_ast_descendente;
 import java.io.FileReader;
 public class Main{
    public static void main(String[] args) throws Exception {
-      ConstructorASTsEval asint = new ConstructorASTsEval(new FileReader(args[0]));
+      ConstructorASTsTiny asint = new ConstructorASTsTiny(new FileReader("D:/Clase/CUARTO/PL/Procesadores-Lenguajes/pr3/constructor_ats_cup/casos/sample4a.in"));
       asint.disable_tracing();
-      System.out.println(asint.analiza());
+      // asint.analiza();
+      asint.analiza().imprime();
    }
 }
