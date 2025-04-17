7117cc7601f01cbf37255902348595c7dd1121a1
SuperDanUCM <117196575+DanielLopezUCM@users.noreply.github.com>
2025-04-18 01:13:56 +0200

    Creada la clase de error de vinculacion

---
+++ pr4/entrega/errores_procesamiento/ErrorVinculacion.java
@@ -0,0 +1,44 @@
+package errores_procesamiento;
+
+public class ErrorVinculacion extends ErrorProcesamiento {
+    TipoErrorVinculacion tipo;
+    String id;
+
+    public enum TipoErrorVinculacion {
+        ERROR_NO_DECLARADO("identificador no declarado"),
+        ERROR_DUPLICADO("declaracion duplicada"),;
+
+        private final String descripcion;
+
+        TipoErrorVinculacion(String descripcion) {
+            this.descripcion = descripcion;
+        }
+
+        @Override
+        public String toString() {
+            return descripcion;
+        }
+    }
+
+    public static ErrorVinculacion errorNoDeclarado(int fila, int columna, String id) {
+        return new ErrorVinculacion(fila, columna, TipoErrorVinculacion.ERROR_NO_DECLARADO, id);
+    }
+
+    public static ErrorVinculacion errorDuplicado(int fila, int columna, String id) {
+        return new ErrorVinculacion(fila, columna, TipoErrorVinculacion.ERROR_DUPLICADO, id);
+    }
+
+    private ErrorVinculacion(int fila, int columna, TipoErrorVinculacion tipo, String id) {
+        super(fila, columna);
+        this.tipo = tipo;
+        this.id = id;
+    }
+
+    public String toStringJuez() {
+        return "Errores_vinculacion fila:" + fila + " col" + columna;
+    }
+
+    public String toStringHumano() {
+        return fila + "," + columna + ":" + tipo + ":" + id;
+    }
+}
